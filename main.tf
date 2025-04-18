95c85bba76015444432eea7fab563fb361693bd0
kaushiktd <kaushiktd@gmail.com>
2025-04-18 11:48:18 +0530

    ssh key creation added

--- main.tf
+++ main.tf
@@ -69,17 +69,42 @@ resource "aws_security_group" "ec2_sg" {
   
 }
 
+
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "github-ec2-deploy-key"
+  public_key = tls_private_key.ssh.public_key_openssh
+
+}
+
 resource "aws_instance" "ec2_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   user_data              = file("user_data.sh")
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
+  key_name               = aws_key_pair.deployer.key_name
+  associate_public_ip_address = true
+
   tags = {
     Name = var.instance_name
   }
 }
 
+resource "aws_ssm_parameter" "ssh_private_key" {
+  name        = "ec2-ssh-private-key"
+  type        = "SecureString"
+  value       = tls_private_key.ssh.private_key_pem
+  description = "Private key to SSH into EC2 from GitHub Actions"
+  overwrite   = true
+}
+
 output "ec2_public_ip" {
   value = aws_instance.ec2_instance.public_ip
 }
+
+
